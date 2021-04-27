31ac2dba0e3da204659b12cccd6dc5f2819bdfff
unknown <72527818+matthewnelmark@users.noreply.github.com>
2021-04-26 20:32:26 -0500

    Edited values (removed assuming sentence)
    just leftover stuff from last lab

--- instruction_memory.vhd
+++ instruction_memory.vhd
@@ -15,12 +15,12 @@ architecture struc_behaviour of instruction_memory is
 	constant NOP : std_logic_vector(31 downto 0) := (others => '1');
 	
 	constant im0 : std_logic_vector(31 downto 0) := NOP;
-	constant im1 : std_logic_vector(31 downto 0) := "000000"&"00001"&"00100"&"00010"&"00000"&"100000";		-- ADD R2, R1, R4				Expect: 6 (This is assuming there is no pipeline.)
-	constant im2 : std_logic_vector(31 downto 0) := "000000"&"00010"&"00101"&"01100"&"00000"&"100010";		-- SUB R12, R2, R5			Expect: 0 (This is assuming there is no pipeline.)
-	constant im3 : std_logic_vector(31 downto 0) := "000000"&"00110"&"00010"&"01110"&"00000"&"100100";		-- AND R14, R6, R2			Expect: 9 (This is assuming there is no pipeline.)
-	constant im4 : std_logic_vector(31 downto 0) := "000000"&"01100"&"01110"&"01111"&"00000"&"100100";		-- AND R15, R12, R14			Expect: 6 (This is assuming there is no pipeline.)
-	constant im5 : std_logic_vector(31 downto 0) := "000000"&"01110"&"01001"&"01111"&"00000"&"100101";		-- OR R15, R14, R9			Expect: 11 (This is assuming there is no pipeline.)
-	constant im6 : std_logic_vector(31 downto 0) := "100011"&"01100"&"00110"&"0000000000000010";				-- LW R6,2(R12)				Expect: 0 (This is assuming there is no pipeline.)
+	constant im1 : std_logic_vector(31 downto 0) := "000000"&"00001"&"00100"&"00010"&"00000"&"100000";		-- ADD R2, R1, R4				Expect: 6 
+	constant im2 : std_logic_vector(31 downto 0) := "000000"&"00010"&"00101"&"01100"&"00000"&"100010";		-- SUB R12, R2, R5			Expect: 1 
+	constant im3 : std_logic_vector(31 downto 0) := "000000"&"00110"&"00010"&"01110"&"00000"&"100100";		-- AND R14, R6, R2			Expect: 4 
+	constant im4 : std_logic_vector(31 downto 0) := "000000"&"01100"&"01110"&"01111"&"00000"&"100100";		-- AND R15, R12, R14			Expect: 0 
+	constant im5 : std_logic_vector(31 downto 0) := "000000"&"01110"&"01001"&"01111"&"00000"&"100101";		-- OR R15, R14, R9			Expect: 15 
+	constant im6 : std_logic_vector(31 downto 0) := "100011"&"01100"&"00110"&"0000000000000010";				-- LW R6,2(R12)				Expect: 8 
 	constant im7 : std_logic_vector(31 downto 0) := NOP;
 	constant im8 : std_logic_vector(31 downto 0) := NOP;
 	constant im9 : std_logic_vector(31 downto 0) := NOP;
