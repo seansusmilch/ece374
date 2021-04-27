b7c2745a430cc446ff2d73add12ce66ffd87162d
unknown <72527818+matthewnelmark@users.noreply.github.com>
2021-04-26 22:45:46 -0500

    Update haz_det.vhd
    Added previously missing logic to haz_det :100:

--- haz_det.vhd
+++ haz_det.vhd
@@ -4,17 +4,20 @@ use work.components.all;
 
 entity haz_det is
 	port( 
-			ID_EX_MemRead : in std_logic;
+			ID_EX_MemRead  : in std_logic;
+			ID_EX_rt_addr : in std_logic_vector(3 downto 0);
+			instr_from_im, IF_ID_instr : in std_logic_vector(31 downto 0);
 			
-			pc_write, IF_ID_write, mux_en : out std_logic;
+			stall : out std_logic;
 end haz_det;
 
 architecture det_logic of haz_det is 
 
-	if (ID/EX.MemRead and ((ID/EX.RegisterRt=IF/ID.RegisterRs) or (ID/EX.RegisterRt=IF/ID.RegisterRt)))
+	if (ID/EX.MemRead and ((ID/EX.RegisterRt=IF/ID.RegisterRs) or (ID/EX.RegisterRt=IF/ID.RegisterRt))) then
+		stall = 1;
 		
-		--then stall
-		
+	else stall = 0;
+	
 end det_logic
 	
 			
